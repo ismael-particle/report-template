1be6d002fcbc1f694c1cee00e8d1ae243d8c9a17
IsmaelSB <78989860+ismael-particle@users.noreply.github.com>
2022-08-31 14:20:27 -0500

    Update section-format.js

--- docs/js/section-format.js
+++ docs/js/section-format.js
@@ -1,4 +1,29 @@
 /**********************************************************
+    Global constants
+************************************************************/
+//icons resources
+const icon_dowload = "https://ismael-particle.github.io/report-template/img/download.png"; 
+const icon_docs = "https://ismael-particle.github.io/report-template/img/docs.png";
+const icon_support = "https://ismael-particle.github.io/report-template/img/support.png";
+
+const color_red     = "#F45151";
+const color_orange  = "#FF993D";
+const color_yellow  = "#FAD51D";
+const color_green   = "#3AD599";
+
+/**********************************************************
+    Building charts
+************************************************************/
+function plotty_chart(data_array){
+    this.x_data = new Array();
+    this.y_data = new Array();
+    this.color = new Array();
+    this.chart = new Array();
+    this.data = data_array
+    this.layout = new Array();
+}
+
+/**********************************************************
     Creat CSV file
 ************************************************************/
   //trigger download of data.csv file
@@ -33,15 +58,6 @@ function creat_table (table_parameters,html_id){
     });
     return table;           
 }
-
-
-/**********************************************************
-    Global constants
-************************************************************/
-//icons resources
-const icon_dowload = "https://ismael-particle.github.io/report-template/img/download.png"; 
-const icon_docs = "https://ismael-particle.github.io/report-template/img/docs.png";
-const icon_support = "https://ismael-particle.github.io/report-template/img/support.png";
 
 /**********************************************************
     Building sections
