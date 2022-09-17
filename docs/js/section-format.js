cfa25a307f0a2d82d92f1d8ebfb8e6b6031c0c8d
IsmaelSB <78989860+ismael-particle@users.noreply.github.com>
2022-09-16 19:53:54 -0500

    Update section-format.js

--- docs/js/section-format.js
+++ docs/js/section-format.js
@@ -13,6 +13,55 @@ const color_green   = "#3AD599";
 const color_blue    = "#00E1FF";
 
 /**********************************************************
+ * Build consts for cards
+ * *******************************************************/
+const card_actions_background = {
+    0: color_green,
+    1: color_yellow,
+    2: color_orange,
+    3: color_red,
+    4: color_blue
+}
+
+function generate_action_color(action_number) {
+  return card_actions_background[ action_number ] || card_actions_background[4];
+}
+
+const card_actions_texts = {
+    0: "Good",
+    1: "Fair",
+    2: "Keep Watch",
+    3: "Take Action",
+    4: "Error"
+}
+
+function generate_action_text(action_number) {
+  return card_actions_texts[ action_number ] || card_actions_texts[4];
+}
+
+/************************************************************
+ * Random html ID cards generation
+ * *********************************************************/
+
+// dec2hex :: Integer -> String
+// i.e. 0-255 -> '00'-'ff'
+function dec2hex (dec) {
+  return dec.toString(16).padStart(2, "0")
+}
+
+// generateId :: Integer -> String
+function generateId (len) {
+  var arr = new Uint8Array((len || 40) / 2)
+  window.crypto.getRandomValues(arr)
+  return Array.from(arr, dec2hex).join('')
+}
+
+function generate_card_id(){
+    return "card_id_" + generateId(20);
+}
+
+
+/**********************************************************
     Building charts
 ************************************************************/
 function generate_id(text,type){
@@ -534,8 +583,64 @@ function creat_section(data_id,colection_data,html_element){
             
             Plotly.newPlot( generate_id(content["section_id"],"chart_2_"), chart_bar_data, layout, {responsive: true, displaylogo: false} );
             break;
-
     }
+}
+
+
+function creat_cards(colection_data,html_element){
+    //Content array
+    //console.log(colection_data);
+
+    var html_cards = '';
+
+    var card_id_html_array = new Array();
+
+    colection_data.forEach( function(item,index){
+        var card_total = item.total;
+
+        if (card_total != 0 && card_total != '0') {
+            var card_title = item.title;
+            var card_main_quantity = item.main_quantity;
+            var card_porcentaje = ((card_main_quantity/card_total)*100).toFixed(2);
+            var card_color = generate_action_color(item.status);
+            var card_action_text = generate_action_text(item.status);
+            var card_html_id = generate_card_id();
+
+            card_id_html_array.push({card_id_html:card_html_id,card_id_porcentage:card_porcentaje});
+        
+            html_cards += `<div class='card_general_box'><div class='card_action_box' style='background-color:${card_color};'><p class='card_action_font'>${card_action_text}</p></div>`;
+            html_cards += `<div class='card_container_box'><div class='card_title_box'> <p class='card_title_font'>${card_title}</p> </div>`;
+            html_cards += `<div class='card_chart_box'><div id='${card_html_id}'><vc-donut v-bind='props'></vc-donut></div></div><div class='card_empty_box'></div>`;
+            html_cards += `<div class='card_quantity_box'><p class='card_quantity_font'>${card_main_quantity}</p></div>`;        
+            html_cards += `<div class='card_detail_box'><p class='card_detail_font'>${card_porcentaje}% of ${card_total} total devices</p>  </div></div></div>`;
+        }
+
+    });
+
+    html_element.insertAdjacentHTML('beforebegin',html_cards);
+    
+
+    Vue.use(vcdonut.default);
+
+    card_id_html_array.forEach( function(card,index){
+
+        new Vue({
+          el: "#"+card.card_id_html,
+          data() {
+            return {
+              props: {
+                size: 130,
+                sections: [
+                    { value: parseInt(card.card_id_porcentage), color: '#00E1FF' }
+                  ],
+                thickness: 30, // set this to 100 to render it as a pie chart instead
+                hasLegend: false,
+                // specify more props here
+              }
+            };
+          }
+        });
+    });
 
 }
 
